631553c613a37b08af22128ab1b3b17b1db942db
brwei01 <bwei01@bbk.ac.uk>
2024-06-09 22:40:37 +0100

    corrected wrong var name

--- CalculateArea/calculate_Area.js
+++ CalculateArea/calculate_Area.js
@@ -5,7 +5,7 @@ function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
-    let area = length * widths;
+    let area = length * width;
 }
 
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
