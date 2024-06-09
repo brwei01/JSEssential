fdbe4df43dc401293132c65f5ddd8294e4b9c5a6
brwei01 <bwei01@bbk.ac.uk>
2024-06-09 16:44:37 +0100

    examine block-level variables

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -14,3 +14,7 @@ const globalConst = "I am a global constant";
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+//Block Scope
+console.log(blockVar);
+console.log(blockLet);
