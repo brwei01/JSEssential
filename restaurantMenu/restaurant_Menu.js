34fcf429e31e5d963173e906df00921087208ec1
brwei01 <bwei01@bbk.ac.uk>
2024-06-11 14:49:46 +0100

    added price to menu

--- restaurantMenu/restaurant_Menu.js
+++ restaurantMenu/restaurant_Menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancake','Eggs Benedict','Oatmeal','Frittata'];
-const mainCourseMenu = ['Steak','Pasta','Burger','Salmon'];
-const dessertMenu = ['Cake','Ice Cream','Pudding','Fruit Salad'];
+const breakfastMenu = ['Pancake - $12','Eggs Benedict - $22.99','Oatmeal - $21.99','Frittata - $15'];
+const mainCourseMenu = ['Steak - $35','Pasta - $24.99','Burger - $30','Salmon - $28'];
+const dessertMenu = ['Cake - $11','Ice Cream - $8','Pudding - $5','Fruit Salad - $10'];
 
 // using map() to manipulate breakfast menu (iterates through)
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index+1}: ${item}</p>`).join('');
