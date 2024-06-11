2da401000590a7c38c2e3831fa920be8c93df4d7
brwei01 <bwei01@bbk.ac.uk>
2024-06-11 17:31:23 +0100

    added function find specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,8 @@
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department:'IT', salary: 50000},
-    {id: 2, name: 'Alice Smith', age: 28, department:'HR', salary: 45000},
-    {id: 3, name: 'Bob Johnson', age: 35, department:'Finance', salary:60000},
+    {id: 1, name: 'John Doe', age: 30, department:'IT', salary: 50000, speicalization: 'JavaScript'},
+    {id: 2, name: 'Alice Smith', age: 28, department:'HR', salary: 45000, specialization: 'Python'},
+    {id: 3, name: 'Bob Johnson', age: 35, department:'Finance', salary:60000, specialization: 'Java'},
+    {id: 4, name: 'J Wong', age: 25, department:'UI', salary: 50000, speicalization: 'JavaScript'},
 ];
 
 // function to display all employees
@@ -29,7 +30,20 @@ function findEmployeeById(employeeId) {
     if (foundEmployee) {
         document.getElementById('employeeDetails').innerHTML=`<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} -  $${foundEmployee.salary}</p>`;
     } else {
-        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID.';
+        document.getElementById('employeeDetails').innerHTML = 'no employee has been found with this ID.';
     }
+}
+
+// function to display employees' details based on specialization
+function findBySpecJS() {
+    const foundEmployee = employees.filter(employee => employee.specialization === 'JavaScript');
+    
+    if (foundEmployee) {
+        const foundEmployeeDisplay = foundEmployee.map(foundEmployee => `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`).join('');
+        document.getElementById('employeeDetails').innerHTML = foundEmployeeDisplay;
+    } else {
+        document.getElementById('employeeDetails').innerHTML = 'no employee has been found with this ID.';
+    }
+    
 }
 
