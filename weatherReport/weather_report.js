ee9fcd1c8496e07068e4bcb84cb649fc491b82c2
brwei01 <bwei01@mail.bbk.ac.uk>
2024-07-27 03:13:33 +0100

    added js

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,19 @@
+function showweatherDetails(event){
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '2a39e1ab327ca8f33c34c1730c824b11';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                    <p>Temperature: ${data.main.temp} &#8451; </p>
+                                    <p>Weather: ${data.weather[0].description}</p>`;
+        })
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+
