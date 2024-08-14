13dd1974627082d433a30c1f87ce1c6b8c78e739
brwei01 <bwei01@mail.bbk.ac.uk>
2024-08-14 01:19:01 +0100

    fixed typo

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -73,15 +73,15 @@ function searchCondition(){
         .then(data => {
             const condition = data.conditions.find(item => item.name.toLowerCase() === input);
             if (condition) {
-                const symptoms = data.condition.symptoms.join(',');
-                const prevention = data.condition.prevention.join(',');
+                const symptoms = condition.symptoms.join(',');
+                const prevention = condition.prevention.join(',');
                 const treatment = condition.treatment;
     
                 resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
                 resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`
                 
                 resultDiv.innerHTML += `<p><strong>Symptoms: </strong>${symptoms}</p>`
-                resultDiv.innerHTML += `<p><strong>Prevention</strong>${prevention}</p>`
+                resultDiv.innerHTML += `<p><strong>Prevention: </strong>${prevention}</p>`
                 resultDiv.innerHTML += `<p><strong>Treatment: </strong>${treatment}</p>`
             } else {
                 resultDiv.innerHTML = 'Condition not found.';
